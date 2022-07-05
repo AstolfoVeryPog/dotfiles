fafb46f39341590e8d15ef958d752433115bb4c8
AstolfoVeryPog <94145691+AstolfoVeryPog@users.noreply.github.com>
2022-07-06 00:41:58 +0200

    add files

---
+++ scripts/full_install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+paru --noconfirm -S kitty i3-gaps dunst rofi polybar picom feh playerctl pavucontrol ttf-jetbrains-mono ttf-font-awesome nerd-fonts-complete
+./install.sh
---
+++ scripts/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mv cfg/i3 ~/.config
+mv cfg/polybar ~/.config
+mv cfg/kitty ~/.config
+mv cfg/dunst ~/.config
+mv cfg/rofi ~/.config
+mv cfg/picom.conf ~/.config
